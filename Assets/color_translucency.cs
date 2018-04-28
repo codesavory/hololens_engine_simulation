a3ee5888a355e86ec604f0d4c0cdbb748deea86a
codesavory <surya.dakshina@gmail.com>
2018-04-28 09:51:53 +0530

    Initial Commit

---
+++ Assets/color_translucency.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class color_translucency : MonoBehaviour {
+    private Color originalColor;
+
+    // Use this for initialization
+    void Start()
+    {
+        var newColor = new Color(Random.Range((float)0.0, (float)1.0), Random.Range((float)0.0, (float)1.0), Random.Range((float)0.0, (float)1.0));
+        this.GetComponent<Renderer>().material.color = newColor;
+        //originalColor = this.GetComponent<Renderer>().material.color;
+
+        originalColor = newColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnSelect()
+    {
+        var cube = this.GetComponent<Renderer>();
+        cube.material.color = Color.magenta;
+    }
+
+    void OnReset()
+    {
+        var cube = this.GetComponent<Renderer>();
+        cube.material.color = originalColor;
+    }
+}
---
+++ Assets/cursor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cursor : MonoBehaviour {
+    private MeshRenderer meshRenderer;
+    //public GameObject FocusedObject { get; private set; }
+    // Use this for initialization
+    void Start()
+    {
+        meshRenderer = this.gameObject.GetComponentInChildren<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var headPosition = Camera.main.transform.position;
+        var gazeDirection = Camera.main.transform.forward;
+
+        RaycastHit hitInfo;
+        if (Physics.Raycast(headPosition, gazeDirection, out hitInfo))
+        {
+            System.Diagnostics.Debug.WriteLine("Hit Enabled");
+            meshRenderer.enabled = true;
+            this.transform.position = hitInfo.point;
+            //this.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+
+            /*var newFocusedObject = hitInfo.collider.gameObject;
+            if (FocusedObject != null && newFocusedObject != FocusedObject)
+            {
+                FocusedObject.SendMessage("OnReset");
+            }
+
+            FocusedObject = newFocusedObject;
+            FocusedObject.SendMessage("OnSelect");*/
+        }
+        else
+        {
+            System.Diagnostics.Debug.WriteLine("Hit Disabled");
+            meshRenderer.enabled = false;
+            /*if (FocusedObject != null)
+            {
+                FocusedObject.SendMessage("OnReset");
+            }
+            FocusedObject = null;*/
+        }
+    }
+}
